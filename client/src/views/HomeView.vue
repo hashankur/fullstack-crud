019ed0f5f21e19e78ce1150548678e405e44b066
Hashan Kurukulasuriya <aeonian.adroit24@gmail.com>
2024-06-02 20:52:29 +0530

    feat(client): add Firebase auth

---
+++ client/src/views/HomeView.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>Feed</h1>
+  <h3>This page is for users only</h3>
+</template>
+
+<script setup>
+import firebase from '../firebase'
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
+import { useRouter } from 'vue-router'
+import { onBeforeUnmount } from 'vue'
+
+const router = useRouter()
+const auth = getAuth(firebase)
+
+const authListener = onAuthStateChanged(auth, (user) => {
+  if (!user) {
+    // not logged in
+    alert('you must be logged in to view this. redirecting to the home page')
+    router.push('/sign-in')
+  }
+})
+onBeforeUnmount(() => {
+  // clear up listener
+  authListener()
+})
+</script>
---
+++ client/src/views/SignUp.vue
@@ -0,0 +1,30 @@
+<template>
+  <h1>Create an Account</h1>
+  <p><input type="text" placeholder="Email" v-model="email" /></p>
+  <p><input type="password" placeholder="Password" v-model="password" /></p>
+  <p><button @click="register">Submit</button></p>
+</template>
+
+<script setup type="ts">
+import { ref } from 'vue'
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import firebase from '../firebase'
+import { useRouter } from 'vue-router' // import router
+
+const email = ref('')
+const password = ref('')
+const router = useRouter() // get a reference to our vue router
+const auth = getAuth(firebase);
+
+const register = () => {
+    createUserWithEmailAndPassword(auth,email.value, password.value) // need .value because ref()
+    .then((data) => {
+      console.log('Successfully registered!')
+      router.push('/') // redirect to the feed
+    })
+    .catch((error) => {
+      console.log(error.code)
+      alert(error.message)
+    })
+}
+</script>
